327a576f5269354d61066799deedad4f2dd0b387
Sam <samweldon9393@gmail.com>
2025-06-20 11:55:43 -0400

    Create both pie charts and change colors

--- Love/data_cochran.R
+++ Love/data_cochran.R
@@ -5,24 +5,43 @@ visitation_groups <- data.frame(
     "Group Size" = c(75, 11, 9, 6)
 )
 
-#visitation_groups <- c(
-#    "No Visits" = 75,
-#    "Visited Early" = 11,
-#    "Visited Late" = 9,
-#    "Consistently Visited" = 6
-#)
-misconduct_groups <- c(
-    "No Misconduct" = 69,
-    "Some Misconduct" = 26,
-    "Heavy Misconduct" = 5
+misconduct_groups <- data.frame(
+    "Misconduct Frequency" = c("No Misconduct", "Some Misconduct", "Heavy Misconduct"),
+    "Group Size" = c(69, 26, 5)
 )
 
-# Make pie chart
+visit_colors <- c(
+  "No Visits" = "#d73027",
+  "Visited Early" = "#fc8d59",
+  "Visited Late" = "#91bfdb",
+  "Consistently Visited" = "#4575b4"
+)
+
+misconduct_colors <- c(
+  "No Misconduct" = "#d73027",
+  "Some Misconduct" = "#fc8d59",
+  "Heavy Misconduct" = "#4575b4"
+)
+
+
+# Make visit pie chart
 p <- ggplot(visitation_groups, aes(x = "", y = Group.Size, fill = Visit.Frequency)) +
   geom_bar(stat = "identity", width = 1) +        # Create bar chart with one bar
   coord_polar("y", start = 0) +                   # Turn bar chart into pie chart
-  labs(title = "Inmate Visitation Groupings") +
+  scale_fill_manual(values = visit_colors) +
+  labs(title = "Visitation Frequency") +
   theme_void() +                                  # Remove axes and background
   theme(legend.title = element_blank())           # Remove legend title
 
 ggsave("visitation_pie_chart.png", plot = p, width = 6, height = 6)
+
+# Make pie chart
+p <- ggplot(misconduct_groups, aes(x = "", y = Group.Size, fill = Misconduct.Frequency)) +
+  geom_bar(stat = "identity", width = 1) +        # Create bar chart with one bar
+  coord_polar("y", start = 0) +                   # Turn bar chart into pie chart
+  scale_fill_manual(values = misconduct_colors) +
+  labs(title = "Misconduct Frequency") +
+  theme_void() +                                  # Remove axes and background
+  theme(legend.title = element_blank())           # Remove legend title
+
+ggsave("misconduct_pie_chart.png", plot = p, width = 6, height = 6)
