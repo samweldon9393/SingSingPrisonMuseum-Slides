8843d3143f434ce85bcc1e803a34012ddbcb7f71
Sam <samweldon9393@gmail.com>
2025-06-24 16:59:27 -0400

    start parsing html

--- Forgiveness/scraper.py
+++ Forgiveness/scraper.py
@@ -3,12 +3,26 @@ from bs4 import BeautifulSoup
 import json
 
 base = "https://ir.lawnet.fordham.edu/trans/"
+url = base + "1"
+response = requests.get(url)
 
+html = response.text
+soup = BeautifulSoup(html, "html.parser")
+
+alpha = soup.body.find(id="alpha")
+
+for child in alpha.children:
+    for s in child.strings:
+        print(s)
+        ''' printing 3 blank lines in between, can we delimit with that? '''
+
+
+'''
 for i in range(1, 201):
     url = base + str(i)
 
     response = requests.get(url)
 
-    print(response.status_code)
     if response.status_code != 200:
         print("Failed to get url")
+'''
