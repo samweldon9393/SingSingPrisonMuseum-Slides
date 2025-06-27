ad163caccc274bd3d435a7b6f07a934696bae839
Sam <samweldon9393@gmail.com>
2025-06-27 15:01:52 -0400

    Add filenames to data

---
+++ Forgiveness/add_filenames.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import json
+
+def make_filename(code: str, date: str):
+    return f"{code}-{date}.pdf"
+
+
+df = pd.read_json("data.json")
+df = df.T
+
+#print(df['Parole Applicant Code'])
+#print(df['Interview/Decision Date'])
+
+df["Filename"] = df.apply(lambda row: make_filename(row["Parole Applicant Code"], row["Interview/Decision Date"]), axis=1)
+
+df.to_json("data.json", orient="records", lines=True)
