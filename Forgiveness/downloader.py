fde5b979aafa9244625a2792d2cea6a0089514b4
Sam <samweldon9393@gmail.com>
2025-06-27 10:46:03 -0400

    download pdfs working

--- Forgiveness/downloader.py
+++ Forgiveness/downloader.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import re
 from bs4 import BeautifulSoup
 
 url = "https://ir.lawnet.fordham.edu/trans/"
@@ -9,4 +10,15 @@ soup = BeautifulSoup(resp.text, "html.parser")
 
 pdf_link = soup.find_all("a", href=True, string="PDF")
 
-print(len(pdf_link))
+
+if pdf_link:
+    for link in pdf_link:
+        r = re.search(r".*? - (.*?)\)\s", link["aria-label"])
+        match = r[1]
+        filename = match.replace(r" (", "-")
+        filename = filename + ".pdf"
+        pdf_url = link["href"]
+        pdf_response = requests.get(pdf_url)
+
+        with open(os.path.join("transcripts", filename), "wb") as f:
+            f.write(pdf_response.content)
