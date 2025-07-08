7559460e62eae0476a97a1c27fc3bb6a4a337249
Sam <samweldon9393@gmail.com>
2025-07-08 12:54:12 -0400

    start building line graph

--- Love/animation/Love/main.py
+++ Love/animation/Love/main.py
@@ -41,17 +41,48 @@ class PieChart(VGroup):
 class WholeScene(Scene):
 
     def construct(self):
+        '''
+        intro = Text("The importance of connection in prison", font_size=18, font="Monument Grotesk")
+        self.play(Write(intro))
+        self.wait(5)
+        self.play(FadeOut(intro))
+
         self.title_sequence("How  many  visits do inmates get?", 18)
-        self.make_pie_chart()
+        visit_data = {
+            "Many": 5,
+            "Some": 20,
+            "None": 75
+        }
+        visit_text = "~75% of inmates studied got no visits"
+        self.make_pie_chart(visit_data, visit_text)
         Scene.clear(self)
+
         self.title_sequence("How many infractions do inmates get?", 13)
+        misconduct_data = {
+            "Many": 5,
+            "Some": 26,
+            "None": 69
+        }
+        misconduct_text = "~69% of inmates studied had no infractions"
+        self.make_pie_chart(misconduct_data, misconduct_text)
+        Scene.clear(self)
+
+        self.title_sequence("Is there a connection between visits and misconduct?", 13)
+        expl = self.explanation_text()
+        self.play(expl)
+        self.wait(10)
+        self.play(FadeOut(expl))
+
+        '''
+
+        self.line_graph()
 
     def title_sequence(self, text, size):
         # Initial large title
         title = Text(text, font_size=size, font="Monument Grotesk")
 
         # Step 1: Animate it onto the center
-        self.play(Write(title))
+        self.play(FadeIn(title))
         self.wait(1.5)  # Pause to linger
 
         # Step 2: Shrink and move to top
@@ -60,16 +91,11 @@ class WholeScene(Scene):
         self.play(Transform(title, title_target))
         self.wait(0.5)
 
-    def make_pie_chart(self):
-        data = {
-            "Many": 5,
-            "Some": 20,
-            "None": 75
-        }
+    def make_pie_chart(self, data, text):
 
         pie_chart = PieChart(data)
         pie_chart.move_to(ORIGIN)
-        pie_chart.labels[0].shift(DOWN*.04)
+        pie_chart.labels[0].shift(DOWN*.035)
         pie_chart.labels[-1].move_to(ORIGIN).shift(DOWN*.3)
 
         for wedge, label in zip(pie_chart.slices, pie_chart.labels):
@@ -81,14 +107,10 @@ class WholeScene(Scene):
 
         self.wait(2)
 
-        for i, d in enumerate(data):
-            if i < len(data) - 1:
-                self.play(
-                        FadeOut(pie_chart.slices[i]),
-                        FadeOut(pie_chart.labels[i])
-                )
-                self.wait(1)
-        msg = Text("~75% of inmates studied had no visitors", font_size=14, font="Monument Grotesk")
+        small_slices = VGroup(pie_chart.slices[0], pie_chart.slices[1], pie_chart.labels[0], pie_chart.labels[1])
+        self.play(FadeOut(small_slices))
+        self.wait(1)
+        msg = Text(text, font_size=14, font="Monument Grotesk")
         bg = BackgroundRectangle(msg, fill_opacity=0.5, fill_color=BLACK, buff=0.1)
         group = VGroup(bg, msg)
 
@@ -100,4 +122,37 @@ class WholeScene(Scene):
                 FadeOut(pie_chart.slices[-1])
         )
 
+    def explanation_text(self):
+        text1 = Text("A 2012 study by Joshua Cochran of FSU College of Criminology", font_size=8, font="Ubuntu Mono")
+        text2 = Text("compared the frequency an inmate was visited over a one year", font_size=8, font="Ubuntu Mono")
+        text3 = Text("period with the number of misconduct citations they received.", font_size=8, font="Ubuntu Mono")
+        text4 = Text("By grouping together inmates with similar visit and infraction", font_size=8, font="Ubuntu Mono")
+        text5 = Text("patterns, the study revealed that inmates who received more", font_size=8, font="Ubuntu Mono")
+        text6 = Text("visits committed less misconduct.", font_size=8, font="Ubuntu Mono")
+        group = VGroup(text1.shift(UP*.45), text2.shift(UP*.3), text3.shift(UP*.15), text4, text5.shift(DOWN*.15), text6.shift(DOWN*.3))
+        return group
 
+    def line_graph(self):
+        plane = NumberPlane(
+            x_range = (0, 6, 1),
+            y_range = (60, 100, 10),
+            x_length = 4,
+            y_length = 2,
+            axis_config={"include_numbers": True},
+        )
+        plane.center()
+
+        #x_values = ["No Visits", "Visited Early", "Visited Late", "Consistently Visited"]
+        x_values = [0, 1, 2, 3, 4]
+        y_values = [70, 80, 90, 100]
+        graph = plane.plot_line_graph(x_values,y_values,z_values=None,line_color=ManimColor('#FFFF00'),add_vertex_dots=True,vertex_dot_radius=0.08,vertex_dot_style=None)
+
+        labels = ["No Visits", "Visited Early", "Visited Late", "Consistently Visited"]
+        x_labels = VGroup()
+
+        for x, label in zip(x_values, labels):
+            text = Text(label, font_size=24).scale(0.4)
+            text.next_to(plane.c2p(x, 30), DOWN)  # c2p = "coordinates to pixel"
+            x_labels.add(text)
+
+        self.play(Create(plane))
